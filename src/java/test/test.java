0ed875e1c9cad3b3a1f7d7233c24e1fbe989b5a7
Kornél <Kornl@Kornel-laptop>
2016-04-26 10:25:26 +0200

    initial commit

---
+++ src/java/test/test.java
@@ -0,0 +1,23 @@
+package test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kornél
+ */
+
+import java.applet.*;
+import java.awt.*;
+
+public class test extends Applet
+{
+   public void paint(Graphics g)
+   {
+      g.drawString("Welcome in Java Applet.",40,20);
+   }
+}
